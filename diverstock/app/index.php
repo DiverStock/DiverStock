48edc146d06e81d33ca3b3cfeac196286787d0e1
DiverStock <familia5figueroa.15@gmail.com>
2019-12-09 14:25:09 -0500

    version 1

---
+++ diverstock/app/index.php
@@ -0,0 +1,35 @@
+<?php include"../confing.php";
+sessionValidate();
+
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<?php include FOLDER_TEMPLATE."head.php";?>
+<body>     
+    <div id="wrapper">
+       <?php include FOLDER_TEMPLATE."top.php";?>
+      <?php rolvalidar(); 
+?>
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                     <h2>Información</h2>   
+                     
+                    </div>
+                </div>              
+                 <!-- /. ROW  -->
+                  <hr />
+              
+                 <!-- /. ROW  -->           
+    </div>
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>  
+        <?php include FOLDER_TEMPLATE."footer.php";?>
+        <?php include FOLDER_TEMPLATE."scripts.php";?>
+</body>
+</html>
